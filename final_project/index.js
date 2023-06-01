03e45e98ee07ab37133c11ff39d365f252aa0d43
Gopagoni Karthik Goud <83135278+wroetoshaw@users.noreply.github.com>
2023-06-02 00:11:51 +0530

    Version 1.0.2

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,28 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req,res,next) {
+    // if user already authorized
+    if (req.session.authorization) {
+        // fetch the access token
+        token = req.session.authorization['accessToken'];
+        // verify the accesstoken
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                // cache the user object in the request
+                req.user = user;
+                next();
+            }
+            //  expired or invalid token
+            else {
+                return res.status(403).json({message: "User not authenticated"})
+            }
+        });
+    }
+    // there has not been a log in even in this session
+    else {
+        return res.status(403).json({message: "User not logged in"})
+    }
 });
  
 const PORT =5000;
